b44a14adcc672ae27718f5441dcdaebea3828ce9
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-11-14 11:23:14 -0700

    Finialized schema.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -27,10 +27,3 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-INSERT INTO department (name) VALUES ('HR'), ('Engineering'), ('Finance');
-INSERT INTO role (title, salary, department_id) VALUES ('Manager', 70000, 1), ('Engineer', 80000, 2), ('Accountant', 60000, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Smith', 2, 1), ('Jim', 'Beam', 3, 1);
-
-ALTER TABLE department ADD UNIQUE (name);
-ALTER TABLE role ADD UNIQUE (title, salary, department_id);
-ALTER TABLE employee ADD UNIQUE (first_name, last_name, role_id, manager_id);
